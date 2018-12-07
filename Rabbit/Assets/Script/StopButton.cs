877157e1688a7a60adf74c8ffdeee0c864b23283
BackChul <qorcjf7409@gmail.com>
2018-12-08 05:45:59 +0900

    no message

---
+++ Rabbit/Assets/Script/StopButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopButton : MonoBehaviour {
+
+    GameObject StopImageObject;
+    GameObject StartButtonObject;
+	void Start () {
+        StopImageObject = GameObject.Find("StopImage");
+        StartButtonObject = GameObject.Find("StartButton");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+}
---
+++ Rabbit/Assets/Script/TouchChangeImage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchChangeImage : MonoBehaviour {
+
+    public Texture[] TextureArray;
+    public bool IsChangeSceneScript;
+    public string ChangeSceneName;
+    private RawImage ImageTexture;
+    private int MaxSize = 0;
+    private int TouchCount = 0;
+    private int OldTouchCount = -1;
+    void Start () {
+        ImageTexture = GetComponent<RawImage>();
+        MaxSize = TextureArray.Length;
+    }
+	
+    void KeyCheck()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ++TouchCount;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        KeyCheck();
+        
+        if(TouchCount >= MaxSize)
+        {
+            if (!IsChangeSceneScript)
+                return;
+            else
+            {
+                GetComponent<SceneChange>().ChangeScene(ChangeSceneName);
+                return;
+            }
+        }
+
+        if(OldTouchCount != TouchCount)
+        {
+            OldTouchCount = TouchCount;
+            ImageTexture.texture = TextureArray[TouchCount];
+        }
+    }
+}
