061c82fbf5b8f646c5a6a6f9675eb5a2322b7e52
정승호 <seungho6858@gmail.com>
2018-12-08 21:15:20 +0900

    [정승호] 랭킹

---
+++ Rabbit/Assets/Scripts/Salmon/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour {
+
+    Text m_input;
+
+    private void Awake()
+    {
+        m_input = transform.Find("Text").GetComponent<Text>();
+    }
+
+    public void Input_Name()
+    {
+        int iScore = Random.Range(0, 500);
+
+        string name = m_input.text;
+
+        Singleton_Manager.GetInstance()._rankManager.Add_Rank(name, (float)iScore);
+    }
+}
---
+++ Rabbit/Assets/Scripts/Salmon/Phase1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Phase1 : MonoBehaviour {
+    
+    public IEnumerator Fade_Out()
+    {
+        transform.Find("InputField").gameObject.SetActive(false);
+
+        Image back = transform.Find("Back").GetComponent<Image>();
+        Text text = transform.Find("Text").GetComponent<Text>();
+        
+        while (true)
+        {
+            back.color -= new Color(0f, 0f, 0f, 1f) * Time.deltaTime / 5f;
+            text.color -= new Color(0f, 0f, 0f, 1f) * Time.deltaTime;
+
+            if (back.color.a < 0f)
+                break;
+
+            yield return null;
+        }
+
+        back.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+
+        
+    }
+}
